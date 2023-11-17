06cba89b5ebea8c2683df939b076f8e7d2fa630c
erincronie <erincronie@gmail.com>
2023-11-16 21:54:37 -0800

    footer complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
         </Routes>
 
-
+        <Footer />
       </BrowserRouter>
 
     </>

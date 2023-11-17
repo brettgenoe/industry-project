771b7c4ff25ce72653917e6bd0fbd3be60e7ecca
Xiaoning Wang <xiaoningwang010314@gmail.com>
2023-11-17 13:10:32 -0500

    movie detail finished

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.scss';
 import Homepage from './pages/Homepage/Homepage';
 import Footer from './components/Footer/Footer';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import MovieDetail from './components/MovieDetail/MovieDetail';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          {/* <Route path="/" element={<Homepage />} />
+          <Route path="/profile" element={<ProfilePage />} /> */}
+          <Route path="/" element={<MovieDetail />} />
         </Routes>
 
         {/* <Footer /> */}

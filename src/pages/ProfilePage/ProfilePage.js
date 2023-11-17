317eb149c90c3e4d41da9dcbfbe3173562d0a481
Kalgi Shah <shahkalgi1997@gmail.com>
2023-11-17 12:44:34 -0700

    Animations, hover effects added

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,25 +1,36 @@
 import "./ProfilePage.scss";
 import { Link, useParams } from "react-router-dom";
-// import BackArrow from "../../assets/icons/BackArrow.png"
-// import Emoji from "../../assets/icons/Emoji.png"
 import data from "../../data/movies.json";
 import ProfilePhoto from "../../assets/icons/Homescreen_Icons/Lizzie.svg"
 import Bell from "../../assets/icons/Homescreen_Icons/notifications-outline.svg"
 import Message from "../../assets/icons/Homescreen_Icons/Chaticon.svg"
 import Emoji from "../../assets/icons/Homescreen_Icons/Bob.svg"
 import BackArrow from "../../assets/icons/FriendsWatchlist_Icons/Backbutton.svg"
+// import Footer from "../../components/Footer/Footer"
+import Favourite from "../../assets/icons/Homescreen_Icons/FAVORITE.svg"
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
 
 const ProfilePage = () => {
 
-console.log(data);
+    const shuffledMovies = shuffleArray([...data]);
 
     return (
         <main className="main">
         <section className="header">
+            <Link to = "/">
             <div className="header__back">
                 <img className = "header__back-icon" src ={BackArrow} alt= "Back Arrow">  
                 </img>
             </div>
+            </Link>
            
             
             <div className="header__image">
@@ -46,15 +57,15 @@ console.log(data);
            </section>
 
            <section className="recent__main">
-            {data.map((movie,index) => (
+            {shuffledMovies.slice(0, 4).map((movie,index) => (
                 <div className="recent__main-movie" key={index}>
                 <img className="recent__main-movie-poster" 
-                        src = {movie.Poster_Link}
-                        alt = "movie-poster"></img>
+                        src = {movie.Poster}
+                        alt = {movie.Title}></img>
                 <article className="recent__main-movie-details">
                     <h4 className="recent__main-movie-details-genre">{movie.Genre}</h4>
-                    <h3 className="recent__main-movie-details-title">{movie.Series_Title}</h3>
-                    <p className="recent__main-movie-details-overview">{movie.Overview}</p>
+                    <h3 className="recent__main-movie-details-title">{movie.Title}</h3>
+                    <p className="recent__main-movie-details-overview">{movie.Plot}</p>
                 </article>
             </div>
             ))}
@@ -70,12 +81,17 @@ console.log(data);
             <section className="friends__main">
             {data.map((movie,index) => (
                 <div key= {index} className="friends__main-movie">
-                    <img className="friends__main-movie-poster" src = {movie.Poster_Link} alt = "movie-poster"></img>
+                        <img className="friends__main-movie-poster" src = {movie.Poster} alt = "movie-poster"></img>
+                        <div className="friends__main-movie-like">
+                            <img className="friends__main-movie-like-icon" src={Favourite} alt= "Favorite"></img>
+                        </div>
+                   
+                    
                     <article className="friends__main-movie-details">
                         
                         <div>
                             <h4 className="friends__main-movie-details-genre">{movie.Genre}</h4>
-                            <h3 className="friends__main-movie-details-title">{movie.Series_Title}</h3>
+                            <h3 className="friends__main-movie-details-title">{movie.Title}</h3>
                         </div>
                         <p className="friends__main-movie-details-options">...</p>
                         
@@ -85,10 +101,7 @@ console.log(data);
             </section>
             
         </section>
-
-
-
-    
+        {/* <Footer /> */}
         </main>
     )
 }
